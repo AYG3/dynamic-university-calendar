a86415a411a690d2b719b54236a69133cac66e26
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-04-08 17:07:15 +0100

    fix: remove unnecessary console log in CalendarGrid for cleaner output

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -115,9 +115,8 @@ const CalendarGrid = ({ events }) => {
   // Generate a mock calendar for the current month (e.g., April 2025)
   const firstDayOfMonth = 2; 
   const weekArr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-  console.log("Events dates: ", events);
 
-  const calendarDays = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+  // const calendarDays = Array.from({ length: daysInMonth }, (_, i) => i + 1);
 
   return (
     <div className="grid grid-cols-7 gap-2 text-center mb-6 text-black">
