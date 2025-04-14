1353204f692a826859df1519e3d7fce80bb46649
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-04-14 14:29:15 +0100

    fix: refactor useApi to encapsulate loading state and event fetching logic

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,23 +2,27 @@ import axios from "axios";
 import { useState } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL;
-export const [loading, setLoading] = useState(false)
 
-export const getEvents = async ( filters = {} ) => {
-    setLoading(true)
-    try {
-        // console.log(API_URL + "?" + query)
-        const response = await axios.get(API_URL, { params: filters })
-        console.log("from API.jsx -  - -  response.data: ", response.data)
-        // console.log("response.url: ", response.config.url)
-    
-        return response.data
-    } catch (error) {
-        console.log("Error getting events: ", error)
-        return []
-    } finally {
-        setLoading(false)
+export const useApi = () => {
+    const [loading, setLoading] = useState(false)
+
+    const getEvents = async ( filters = {} ) => {
+        
+        setLoading(true)
+        try {
+            // console.log(API_URL + "?" + query)
+            const response = await axios.get(API_URL, { params: filters })
+            console.log("from API.jsx -  - -  response.data: ", response.data)
+            // console.log("response.url: ", response.config.url)33 eee2e1a
+            return response.data
+        } catch (error) {
+            console.log("Error getting events: ", error)
+            return []
+        } finally {
+            setLoading(false)
+        }
     }
+    return { loading, getEvents }
 }
 
 export const createEvent = async (eventData) => {
