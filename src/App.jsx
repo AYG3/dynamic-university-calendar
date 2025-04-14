cb2aaf8569d706c201c6dc725ca4ef8d235ce067
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-04-14 11:08:47 +0100

    fix: update routing in App component to include default redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EventCalendar from './pages/Calendar.jsx'
-import Navbar from './pages/Navbar.jsx';
-import Events from './pages/Events.jsx';
-import Event2 from './pages/Event2.jsx';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import EventCalendar from "./pages/Calendar.jsx";
+import Navbar from "./pages/Navbar.jsx";
+import Events from "./pages/Events.jsx";
+import Event2 from "./pages/Event2.jsx";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/calendar' element={<EventCalendar />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/event2' element={<Event2 />} />
+        <Route path="" element={<Navigate to={"/calendar"} />} />
+        <Route path="/calendar" element={<EventCalendar />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/event2" element={<Event2 />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
